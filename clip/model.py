98836bb8dad00dfce6c9534d3497ef2f3515168a
Jash Vira <62268134+jash2001404@users.noreply.github.com>
2023-02-23 09:35:44 +1030

    Debugging statements modified

--- clip/model.py
+++ clip/model.py
@@ -76,37 +76,40 @@ class AttentionPool2d(nn.Module):
         self.num_heads = num_heads
 
     def forward(self, x):
-        # x shape here is ---> 1, 2048, 7, 7
+        # OG: x shape here is ---> 1, 2048, 7, 7
         x = x.flatten(start_dim=2).permute(2, 0, 1)  # NCHW -> (HW)NC
         x = torch.cat([x.mean(dim=0, keepdim=True), x], dim=0)  # (HW+1)NC
         x = x + self.positional_embedding[:, None, :].to(x.dtype)  # (HW+1)NC
 
-        # # temp = self.c_proj(x.transpose(-1, -2) @ self.v_proj(x))
-        # # print(x.shape,x[:1].squeeze(0).shape, temp.shape)
-        # x = self.c_proj(x)
-        # print("shape",x[0].shape)
+        # OG: x shape here is ---> 50, 1, 2048
+        # temp = self.c_proj(x.transpose(-1, -2) @ self.v_proj(x))
+        # print(x.shape,x[:1].squeeze(0).shape, temp.shape)
+        x = self.c_proj(x)
 
-        x, _ = F.multi_head_attention_forward(
-            query=x[:1], key=x, value=x,
-            embed_dim_to_check=x.shape[-1],
-            num_heads=self.num_heads,
-            q_proj_weight=self.q_proj.weight,
-            k_proj_weight=self.k_proj.weight,
-            v_proj_weight=self.v_proj.weight,
-            in_proj_weight=None,
-            in_proj_bias=torch.cat([self.q_proj.bias, self.k_proj.bias, self.v_proj.bias]),
-            bias_k=None,
-            bias_v=None,
-            add_zero_attn=False,
-            dropout_p=0,
-            out_proj_weight=self.c_proj.weight,
-            out_proj_bias=self.c_proj.bias,
-            use_separate_proj_weight=True,
-            training=self.training,
-            need_weights=False
-        )
-        # x shape here is ---> 1, 1, 512
-        return x.squeeze(0)
+        # x shape here should be ---> 50, 1, 512
+
+        # x, _ = F.multi_head_attention_forward(
+        #     query=x[:1], key=x, value=x,
+        #     embed_dim_to_check=x.shape[-1],
+        #     num_heads=self.num_heads,
+        #     q_proj_weight=self.q_proj.weight,
+        #     k_proj_weight=self.k_proj.weight,
+        #     v_proj_weight=self.v_proj.weight,
+        #     in_proj_weight=None,
+        #     in_proj_bias=torch.cat([self.q_proj.bias, self.k_proj.bias, self.v_proj.bias]),
+        #     bias_k=None,
+        #     bias_v=None,
+        #     add_zero_attn=False,
+        #     dropout_p=0,
+        #     out_proj_weight=self.c_proj.weight,
+        #     out_proj_bias=self.c_proj.bias,
+        #     use_separate_proj_weight=True,
+        #     training=self.training,
+        #     need_weights=False
+        # )
+        # OG: x shape here is ---> 1, 1, 512
+        # return x.squeeze(0)
+        return x
 
 
 class ModifiedResNet(nn.Module):
@@ -459,7 +462,9 @@ class CLIP(nn.Module):
         # cosine similarity as logits
         logit_scale = self.logit_scale.exp()
         logits_per_image = logit_scale * image_features @ text_features.t()
-        logits_per_text = logits_per_image.t()
+
+        # logits_per_text = logits_per_image.t()
+        logits_per_text = logits_per_image.transpose(-2,-1)
 
         # shape = [global_batch_size, global_batch_size]
         return logits_per_image, logits_per_text
